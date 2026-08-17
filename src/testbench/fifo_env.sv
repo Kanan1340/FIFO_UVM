19c0b408875badd219de852202cfbe31bf392aee
Kanan1340 <kanansoni134@gmail.com>
2026-08-17 11:09:06 +0530

    modified

--- src/testbench/fifo_env.sv
+++ src/testbench/fifo_env.sv
@@ -4,35 +4,39 @@ class fifo_env extends uvm_env;
 `uvm_component_utils(fifo_env)
 
 fifo_cfg cfg_h;
-fifo_in_agt in_agt_h;
-fifo_out_agt out_agt_h;
+fifo_agt agt_h;
 fifo_sb sb_h;
 fifo_cov cov_h;
 
 function new(string name="fifo_env",uvm_component parent);
-	super.new(name,parent);
+super.new(name,parent);
 endfunction
 
 function void build_phase(uvm_phase phase);
-	super.build_phase(phase);
+super.build_phase(phase);
 
-	if(!uvm_config_db#(fifo_cfg)::get(this,"","cfg",cfg_h))
-	`uvm_fatal(get_type_name(),"Can't get cfg")
+if(!uvm_config_db#(fifo_cfg)::get(this,"","cfg",cfg_h))
+`uvm_fatal(get_type_name(),"Can't get cfg")
 
-	in_agt_h = fifo_in_agt::type_id::create("in_agt_h",this);
-	out_agt_h = fifo_out_agt::type_id::create("out_agt_h",this);
+agt_h=fifo_agt::type_id::create("agt_h",this);
 
-	if(cfg_h.has_sb)
-	sb_h=fifo_sb::type_id::create("sb_h",this);
+if(cfg_h.has_sb)
+sb_h=fifo_sb::type_id::create("sb_h",this);
 
-	if(cfg_h.has_cov)
-	cov_h=fifo_cov::type_id::create("cov_h",this);
+if(cfg_h.has_cov)
+cov_h=fifo_cov::type_id::create("cov_h",this);
+
 endfunction
 
 function void connect_phase(uvm_phase phase);
-	super.connect_phase(phase);
-	agt.mon.ap.connect(sb.act_imp);
-	agt.mon.ap.connect(cov.analysis_export);
+super.connect_phase(phase);
+
+if(cfg_h.has_sb)
+agt_h.mon_h.ap.connect(sb_h.act_imp);
+
+if(cfg_h.has_cov)
+agt_h.mon_h.ap.connect(cov_h.analysis_export);
+
 endfunction
 
 endclass
--- src/testbench/fifo_sb.sv
+++ src/testbench/fifo_sb.sv
@@ -1,88 +1,91 @@
 
 `uvm_analysis_imp_decl(_act)
-`uvm_analysis_imp_decl(_exp)
+
 class fifo_sb extends uvm_scoreboard;
 
 `uvm_component_utils(fifo_sb)
 
-uvm_analysis_imp_act #(fifo_tx,fifo_sb) act_imp;
-uvm_analysis_imp_exp #(fifo_tx,fifo_sb) exp_imp;
+uvm_analysis_imp_act#(fifo_tx,fifo_sb) act_imp;
 
 fifo_tx act_q[$];
 fifo_tx exp_q[$];
+bit [`DW-1:0] q[$];
 
-int match, mismatch;
+int match,mismatch;
 
 function new(string name="fifo_sb",uvm_component parent);
-	super.new(name,parent);
-	act_imp=new("act_imp",this);
-	exp_imp=new("exp_imp",this);
+super.new(name,parent);
+act_imp=new("act_imp",this);
 endfunction
 
 function void write_act(fifo_tx tx);
-	act_q.push_back(tx);
-	check();
+fifo_tx exp;
+act_q.push_back(tx);
+ref_model(tx);
+check();
 endfunction
 
-function void write_exp(fifo_tx tx);
-	exp_q.push_back(tx);
-	check();
+function void ref_model(fifo_tx tx);
+fifo_tx exp;
+
+exp=fifo_tx::type_id::create("exp");
+
+if(tx.rst)begin
+q.delete();
+exp.data_out=0;
+exp.full=0;
+exp.empty=1;
+exp_q.push_back(exp);
+return;
+end
+
+if(tx.wr_cs&&tx.wr_en&&tx.rd_cs&&tx.rd_en)begin
+if(q.size()>0)
+exp.data_out=q.pop_front();
+if(q.size()<`DEPTH)
+q.push_back(tx.data_in);
+end
+else if(tx.wr_cs&&tx.wr_en)begin
+if(q.size()<`DEPTH)
+q.push_back(tx.data_in);
+end
+else if(tx.rd_cs&&tx.rd_en)begin
+if(q.size()>0)
+exp.data_out=q.pop_front();
+end
+
+exp.full=(q.size()==`DEPTH);
+exp.empty=(q.size()==0);
+
+exp_q.push_back(exp);
+
 endfunction
-
-virtual task ref_model(fifo_tx t);
-
-	if(t.rst)begin 
-	   t.wr_cs=0;
-	   t.wr_en=0;
-	   t.rd_cs=0;
-	   t.rd_en=0;
-	   t.data_in=0;
-	   t.data_out=0;
-	   t.full=0;
-	   t.empty=1;
-	end
-	
-	if(t.wr_cs && t.wr_en && t.rd_cs && t.rd_en)begin
-		   out_tx.data_out=q.pop_front();
-			if(q.size()<`DEPTH)
-			q.push_back(tx.data_in);
-	end
-	else if(tx.wr_cs&&tx.wr_en)begin
-		if(q.size()<`DEPTH)
-		q.push_back(tx.data_in);
-	end
-	else if(tx.rd_cs&&tx.rd_en)begin
-		if(q.size()>0)
-		out_tx.data_out=q.pop_front();
-	end
-
-endtask
 
 function void check();
-	fifo_tx act, exp;
-	
-	if(act_q.size()==0 || exp_q.size()==0)
-	return;
+fifo_tx act,exp;
 
-	act=act_q.pop_front();
-	exp=exp_q.pop_front();
+if(act_q.size()==0||exp_q.size()==0)
+return;
 
-	if(act.rd_cs && act.rd_en)begin
-	  // if(act.data_out === exp.data_out)begin
-	   if(exp.compare(act))begin
-		match++;
-		`uvm_info("SB","MATCH",UVM_LOW)
-	   end
-	   else begin
-	   	mismatch++;
-	   	`uvm_error("SB",$sformatf("\n EXP=%0h \n ACT=%0h",exp.data_out,act.data_out))
-	   end
-	end
+act=act_q.pop_front();
+exp=exp_q.pop_front();
+
+if(act.rd_cs&&act.rd_en)begin
+if(act.data_out===exp.data_out)begin
+match++;
+`uvm_info("SB","MATCH",UVM_LOW)
+end
+else begin
+mismatch++;
+`uvm_error("SB",$sformatf("EXP=%0h ACT=%0h",exp.data_out,act.data_out))
+end
+end
+
 endfunction
 
 function void report_phase(uvm_phase phase);
-	super.report_phase(phase);
-	`uvm_info("SB",$sformatf("MATCH=%0d MISMATCH=%0d",match,mismatch),UVM_NONE)
+super.report_phase(phase);
+`uvm_info("SB",$sformatf("MATCH=%0d MISMATCH=%0d",match,mismatch),UVM_NONE)
 endfunction
 
 endclass
